047e7e7f1fccf9056c3e842ad49787bedf2d81ff
Javier Rojas <javierluis15975@hotmail.com>
2022-10-31 19:01:11 -0500

    Archivo-ready

--- Wason-Pagina/Wason.js
+++ Wason-Pagina/Wason.js
@@ -1,9 +1,23 @@
-const burger = document.getElementById("icon-burger-mobile");
-const nav = document.getElementById("cont-nav-mobile");
-const wason = document.getElementById("cont-img-barber");
+const d = document;
 
+const burger = d.getElementById("icon-burger-mobile");
+const nav = d.getElementById("cont-nav-mobile");
 
 
 burger.addEventListener("click", () => {
     nav.classList.toggle("MostrarMenuJs")       
 })  
+
+
+const $closePrecio = d.getElementById("close-precios");
+const $SectionPrecios = d.querySelector(".section-precios")
+const $IconPrecios = d.querySelector(".icon-precios");
+
+d.addEventListener("click", (e) => {
+    if (e.target.matches("#close-precios")) {
+        d.querySelector(".section-precios").classList.add("ClosePreciosJs")
+    }
+    if (e.target.matches(".icon-precios")) {
+        d.querySelector(".section-precios").classList.remove("ClosePreciosJs")
+    }
+})
